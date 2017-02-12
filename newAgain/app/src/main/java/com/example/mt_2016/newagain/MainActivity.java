31a48a3aa1622f11f69fc23f255e03001f504320
Neel <nelesh96@gmail.com>
2017-02-12 16:51:22 +0500

    add instance variabe for use databinding class on xml file

--- newAgain/app/src/main/java/com/example/mt_2016/newagain/MainActivity.java
+++ newAgain/app/src/main/java/com/example/mt_2016/newagain/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.mt_2016.newagain;
 
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mt_2016.newagain.databinding.BindingClass;
+
 public class MainActivity extends AppCompatActivity {
+
+    BindingClass binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
