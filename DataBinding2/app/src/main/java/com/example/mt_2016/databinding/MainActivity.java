82c2ae7a664af7db27e150ebf8287b8600915092
Neel <nelesh96@gmail.com>
2017-02-12 16:21:44 +0500

    Create DataBinding Class

---
+++ DataBinding2/app/src/main/java/com/example/mt_2016/databinding/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mt_2016.databinding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
